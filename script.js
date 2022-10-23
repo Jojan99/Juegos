e84eadc079017f79b3360b4013bbd3b608823488
Jojan99 <116475795+Jojan99@users.noreply.github.com>
2022-10-23 11:39:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 const ruleta = document.querySelector('#ruleta');
 
 ruleta.addEventListener('click', girar);
-giros = 1;
+giros = 0;
 function girar(){
-  if (giros = 1) {
+  if (giros >= 100){
+ alert ("Ya haz completado todos los tiros")
+  }else{
+  if (giros < 100) {
     let rand = Math.random() * 7200;
     calcular(rand);
     giros++;
+    console.log(giros)
     var sonido = document.querySelector('#audio');
-    sonido.setAttribute('src', 'ruleta.mp3');
+    sonido.setAttribute('src', 'sonido/ruleta.mp3');
   }else{
     Swal.fire({
       icon: 'success',
-      title: 'VUELVA PRONTO EL JUEGO TERMINO <3',
+      title: 'VUELVA PRONTO EL JUEGO TERMINO!!',
       confirmButtonColor: '#3085d6',
       confirmButtonText: 'Aceptar',
       allowOutsideClick: false
     }).then((result)=>{
       if (result.value == true) {
-        giros = 1;    
+        giros = 0;
+         document.querySelector('.elije').innerHTML = 'TU CORTESIA ES: ';   
       }
     })
   }
-
+}
+}
 
 function premio(premios){
 
@@ -37,7 +43,7 @@ function premio(premios){
   }else{
     Swal.fire({
       icon: 'success',
-      title: ' ' + premios,
+      title: 'Eres ganadorww  a de ' + premios,
       confirmButtonColor: '#3085d6',
       confirmButtonText: 'Aceptar',
       allowOutsideClick: false
@@ -50,39 +56,46 @@ function premio(premios){
 
  function calcular(rand) {
 
-  valor = rand / 360;
+   valor = rand / 360;
+
   valor = (valor - parseInt(valor.toString().split(".")[0]))* 360;
   ruleta.style.transform = "rotate("+rand+"deg)";
+
 
   setTimeout(() => {
   switch (true) {
     case valor > 0 && valor <= 45:
-     premio("JOJAN HA GANADO UN BESO DE TUS LABIOS");
+     premio("FRUTA #1");
      break;
      case valor > 45 && valor <= 90:
-     premio("JOJAN HA GANADO UN BESO DE TUS LABIOS");
+     premio("BESO #2");
      break;
      case valor > 90 && valor <= 135:
-     premio("JOJAN HA GANADO UN BESO DE TUS LABIOS"); 
+     premio("NUTELLA #3"); 
      break; 
      case valor > 135 && valor <= 180:
-     premio("JOJAN HA GANADO UN BESO DE TUS LABIOS");
+     premio("FOTO #4");
      break;
      case valor > 180 && valor <= 225:
-     premio("JOJAN HA GANADO UN BESO DE TUS LABIOS");
+     premio("HORNITO #5");
      break; 
      case valor > 225 && valor <= 270:
-     premio("JOJAN HA GANADO UN BESO DE TUS LABIOS");
+     premio("BESO #6");
      break;
      case valor > 270 && valor <= 315:
-     premio("JOJAN HA GANADO UN BESO DE TUS LABIOS");
+     premio("POPSY #7");
      break;
-     case valor > 315 && valor <= 360:
-     premio("JOJAN HA GANADO UN BESO DE TUS LABIOS"); 
+     case valor > 315 && valor <= 336:
+      premio("PERDISTE BONO #8"); 
      break;
+     case valor > 336 && valor <= 360:
+     premio("GANASTE BONO #8"); 
+     break;
+  
+     
   }
 
  }, 5000);
 
 }
-}
+
